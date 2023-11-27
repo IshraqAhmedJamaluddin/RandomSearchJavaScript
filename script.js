396e6b096a2b53156e0724feb7a08edb2de2441d
Ishraq Ahmed <ishraqahmedjamaluddin@gmail.com>
2023-11-27 22:06:52 +0000

    added the second function

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function range(start, end, step) {
+  return Array.from({ length: Math.floor((end - start) / step) + 1 }, (_, index) => start + index * step);
+}
+
 function randomSearch() {
   var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   var target = parseInt(document.getElementById('target').value);
@@ -7,13 +15,18 @@ function randomSearch() {
     return;
   }
 
-  [found, result] = randomSearch_1(list, target)
+  [found, result] = randomSearch_1(list, target);
+  // [found, result] = randomSearch_2(list, target);
 
-  result += "Done!";
+  result += "Done! Found = " + found;
   document.getElementById('result').innerHTML = result;
 }
 
 // complete this function
 function randomSearch_1(myList, target) {
-
 }
+
+// complete this function
+function randomSearch_2(myList, target) {
+}
+
