1ff79e465529becca49ebc80f2e962bb5a715380
Ishraq Ahmed <ishraqahmedjamaluddin@gmail.com>
2023-11-27 21:25:41 +0000

    updated the outside function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function randomSearch() {
-  var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Example list, you can modify this as needed
+  var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   var target = parseInt(document.getElementById('target').value);
 
   if (isNaN(target)) {
@@ -7,13 +7,13 @@ function randomSearch() {
     return;
   }
 
-  var found = false;
-  var result = '';
-  var message = '';
-
-  // Your loop here:
-
+  [found, result] = randomSearch_1(list, target)
 
   result += "Done!";
   document.getElementById('result').innerHTML = result;
+}
+
+// complete this function
+function randomSearch_1(myList, target) {
+
 }
